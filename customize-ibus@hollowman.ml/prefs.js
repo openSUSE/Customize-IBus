3b596f4037d5aaf532c006c1f16415077ecb3d3f
Hollow Man <hollowman@hollowman.ml>
2021-07-07 16:34:54 +0800

    Update Translation
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- customize-ibus@hollowman.ml/prefs.js
+++ customize-ibus@hollowman.ml/prefs.js
@@ -1401,7 +1401,7 @@ const CustomizeIBus = GObject.registerClass(
         new Gtk.Label({
           use_markup: true,
           label: _(
-            '<span size="small">Copyright © 2021 <a href="https://github.com/openSUSE">Hollow Man</a> &lt;<a href="mailto:hollowman@hollowman.ml">hollowman@hollowman.ml</a>&gt;</span>'
+            '<span size="small">Copyright © 2021 <a href="https://github.com/HollowMan6">Hollow Man</a> &lt;<a href="mailto:hollowman@hollowman.ml">hollowman@hollowman.ml</a>&gt;</span>'
           ),
         }),
         0,
