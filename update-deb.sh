773baefdf270c69cc364586187f5286ee8f89ce5
Hollow Man <hollowman@hollowman.ml>
2021-07-15 23:28:04 +0800

    cleanup
    Signed-off-by: Hollow Man <hollowman@hollowman.ml>

--- update-deb.sh
+++ update-deb.sh
@@ -4,10 +4,8 @@
 # Contributor: Hollow Man <hollowman at hollowman dot ml>
 
 cd deb
-KEYNAME='Hollow Man (Domain Address) <hollowman@hollowman.ml>'
-dpkg-sig -k "$KEYNAME" --sign repo gnome-shell-extension-customize-ibus_*.deb
 rm -fr Packages; dpkg-scanpackages . > Packages
 rm -fr Packages.gz; cat Packages | gzip -c9  > Packages.gz
 rm -fr Release; apt-ftparchive release . > Release
-rm -fr Release.gpg; gpg --default-key "$KEYNAME" -abs -o Release.gpg Release
-rm -fr InRelease; gpg --default-key "$KEYNAME" --clearsign -o InRelease Release
+rm -fr Release.gpg; gpg -abs -o Release.gpg Release
+rm -fr InRelease; gpg --clearsign -o InRelease Release
